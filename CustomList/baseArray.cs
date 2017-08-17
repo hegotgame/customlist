58b3cfb4e8a0ed38c20de0a750cb5d85634b3778
hegotgame <jgill@uwm.edu>
2017-08-17 09:47:44 -0500

    initial commit

---
+++ CustomList/baseArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class baseArray
+    {
+        //properties
+            //capacity
+            //count
+            //item[int32]
+        //methods
+        //explicit interface implementations
+        //extension methods
+    }
+}
